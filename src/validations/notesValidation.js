bf7578ef12baff5ca5225006abf8fc83b821f547
vivtorochok <vika17052001@gmail.com>
2026-08-08 11:39:19 +0300

    Update notesValidation.js

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -10,34 +10,77 @@ const objectIdValidator = (value, helpers) => {
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().allow(''),
+    page: Joi.number().integer().min(1).default(1).messages({
+      'number.base': 'Page must be a number',
+      'number.integer': 'Page must be an integer',
+      'number.min': 'Page must be at least {#limit}',
+    }),
+    perPage: Joi.number().integer().min(5).max(20).default(10).messages({
+      'number.base': 'perPage must be a number',
+      'number.integer': 'perPage must be an integer',
+      'number.min': 'perPage must be at least {#limit}',
+      'number.max': 'perPage must be at most {#limit}',
+    }),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .messages({
+        'any.only': `Tag must be one of: ${TAGS.join(', ')}`,
+      }),
+    search: Joi.string().allow('').messages({
+      'string.base': 'Search must be a string',
+    }),
   }),
 };
 
 export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(objectIdValidator).required(),
+    noteId: Joi.string().custom(objectIdValidator).required().messages({
+      'any.required': 'noteId is required',
+    }),
   }),
 };
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
+    title: Joi.string().min(1).required().messages({
+      'string.base': 'Title must be a string',
+      'string.min': 'Title should have at least {#limit} character',
+      'any.required': 'Title is required',
+    }),
+    content: Joi.string().allow('').messages({
+      'string.base': 'Content must be a string',
+    }),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .messages({
+        'any.only': `Tag must be one of: ${TAGS.join(', ')}`,
+      }),
   }),
 };
 
 export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(objectIdValidator).required(),
+    noteId: Joi.string().custom(objectIdValidator).required().messages({
+      'any.required': 'noteId is required',
+    }),
   }),
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
-  }).min(1),
+    title: Joi.string().min(1).messages({
+      'string.base': 'Title must be a string',
+      'string.min': 'Title should have at least {#limit} character',
+    }),
+    content: Joi.string().allow('').messages({
+      'string.base': 'Content must be a string',
+    }),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .messages({
+        'any.only': `Tag must be one of: ${TAGS.join(', ')}`,
+      }),
+  })
+    .min(1)
+    .messages({
+      'object.min':
+        'At least one field (title, content or tag) must be provided',
+    }),
 };
