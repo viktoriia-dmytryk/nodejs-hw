61b853a03054bf5adb40fed35b4414b4b43481d3
vivtorochok <vika17052001@gmail.com>
2026-08-14 21:16:54 +0300

    Homework-4

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -6,7 +6,7 @@ export const getAllNotes = async (req, res) => {
 
   const skip = (page - 1) * perPage;
 
-  const query = Note.find();
+  const query = Note.find({ userId: req.user._id });
 
   if (tag) {
     query.where('tag').equals(tag);
@@ -37,7 +37,7 @@ export const getAllNotes = async (req, res) => {
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findById(noteId);
+  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
@@ -45,13 +45,16 @@ export const getNoteById = async (req, res) => {
 };
 
 export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
+  const note = await Note.create({ ...req.body, userId: req.user._id });
   res.status(201).json(note);
 };
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findOneAndDelete({ _id: noteId });
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+    userId: req.user._id,
+  });
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
@@ -60,9 +63,13 @@ export const deleteNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
-    returnDocument: 'after',
-  });
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId, userId: req.user._id },
+    req.body,
+    {
+      returnDocument: 'after',
+    }
+  );
   if (!note) {
     throw createHttpError(404, 'Note not found');
   }
---
+++ src/validations/authValidation.js
@@ -0,0 +1,26 @@
+import { Joi, Segments } from 'celebrate';
+
+export const registerUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.email': 'Email must be a valid email address',
+      'any.required': 'Email is required',
+    }),
+    password: Joi.string().min(8).required().messages({
+      'string.min': 'Password should have at least {#limit} characters',
+      'any.required': 'Password is required',
+    }),
+  }),
+};
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required().messages({
+      'string.email': 'Email must be a valid email address',
+      'any.required': 'Email is required',
+    }),
+    password: Joi.string().required().messages({
+      'any.required': 'Password is required',
+    }),
+  }),
+};
