f8188e019d5dc87bc91d96e1716d8d4a9c1fa2a0
vivtorochok <vika17052001@gmail.com>
2026-08-23 13:43:39 +0300

    remove

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -7,9 +7,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
 export const sendEmail = async options => {
